d1ebecd105b500ca30bb17a4d4b4e5b480afd5b7
Crowdbotics <team@crowdbotics.com>
2021-08-06 17:07:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "solitary_bonus_29572Identifier",
+  url: "https://solitary_bonus_29572Identifier.botics.co",
+  api: "https://solitary_bonus_29572Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
